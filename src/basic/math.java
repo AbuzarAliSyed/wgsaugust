a6e936941e3efecc878abe33e07f61b16beb39d2
AbuzarAliSyed <Abuzar-Ali.Syed@Worldline.com>
2022-08-18 17:25:20 +0530

    pushed math changed file

--- src/basic/math.java
+++ src/basic/math.java
@@ -7,7 +7,7 @@ public class math {
 	    public static void add(int a,int b,int c) {
 	        System.out.println("Sum is :" + a+b+c);
 	    }
-	    public static void multiply(int a,int b) {
-	        System.out.println("Product is :" + a*b);
+	    public static void subtract(int a,int b) {
+	        System.out.println("Product is :" + a-b);
 	    }
 }
